b6983d5ed356fd4afbfededf51e9214e47525a7e
Paweł Jaśkiewicz <inpersonasua@gmail.com>
2018-01-07 18:46:24 +0100

    Small ammendments - not yet working.

--- catalog_names.py
+++ catalog_names.py
@@ -16,6 +16,7 @@ if len(sys.argv) != 2:
 xlsx_file = sys.argv[1]
 workbook = load_workbook(filename= xlsx_file)
 sheet = workbook.active
+name_library = {}
 
 for row in sheet.iter_rows(max_col=3):
     part_number_dashed = dash(row[0])
@@ -29,8 +30,13 @@ def dash(number):
         return dash(number[:9]) + "-" + number[9:]
 
 def get_short_name(part_number):
-    website = BeautifulSoup(site_url)
-    website.find
+    ''' Return short name for given number.
+        It first checks if number is in name_library dictionary. If not
+        tryes to find in website, also filling name_library with
+        numbers and corresponding short names from the page.'''
+    if name_library.get(part_number)
+        return name_library.get(part_number)
+    soup = BeautifulSoup(site_url)
     return short_name
 
 workbook.save(xlsx_file)
