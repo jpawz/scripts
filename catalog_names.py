de22550c71ac9fedad89d370c6d0d5d87adfd6f5
Paweł Jaśkiewicz <inpersonasua@gmail.com>
2018-01-09 19:02:06 +0100

    catalog_names.py - finished

--- catalog_names.py
+++ catalog_names.py
@@ -5,7 +5,10 @@
 # Output: the same xlsx file with third column filled with
 #         short names
 
-import sys, openpyxl
+import sys
+import openpyxl
+import os
+import re
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 
@@ -14,31 +17,45 @@ if len(sys.argv) != 2:
     exit()
 
 xlsx_file = sys.argv[1]
-workbook = load_workbook(filename= xlsx_file)
+workbook = load_workbook(filename=xlsx_file)
 sheet = workbook.active
 name_library = {}
 
-# TODO: consider other number lengths
+
 def dash(number):
+    number = str(number)
     if len(number) == 9:
         return number[:3] + "-" + number[3:5] + "-" + number[5:]
     if len(number) == 11:
         return dash(number[:9]) + " " + number[9:]
+    return number
+
+
+def fill_library(file):
+    soup = BeautifulSoup(file, "html.parser")
+    naz = soup.find_all(name="td", attrs={"class": "naz"}, )
+    nr = soup.find_all(name="td", attrs={"class": "nr"}, )
+    for i in range(len(nr)):
+        name_library[nr[i].string] = naz[i].string
+
 
 def get_short_name(part_number):
-    ''' Return short name for given number.
-        It first checks if number is in name_library dictionary. If not
-        tryes to find in website, also filling name_library with
-        numbers and corresponding short names from the page.'''
     if name_library.get(part_number):
         return name_library.get(part_number)
-    with open("file.htm",encoding="windows-1250") as fp:
-        soup = BeautifulSoup(fp, "html.parser")
-        naz = soup.find_all(name="td",attrs={"class": "naz"}, )
-        nr = soup.find_all(name="td",attrs={"class": "nr"}, )
-        for i in range(len(nr)):
-            name_library[nr[i].string] = naz[i].string
-    return name_library.get(part_number)
+    return ""
+
+
+def fill_names_library_from_dir():
+    file_pattern = re.compile(r'[0-9]{2}-[0-9]{2}.htm')
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if root.endswith('gb') and file_pattern.match(file):
+                file_path = os.path.join(root, file)
+                with open(file_path, encoding="windows-1250") as fp:
+                    fill_library(fp)
+
+
+fill_names_library_from_dir()
 
 for row in sheet.iter_rows(max_col=3):
     part_number_dashed = dash(row[0].value)
